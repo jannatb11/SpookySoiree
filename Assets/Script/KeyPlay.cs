ad92494fb333be76099ae9e02e76668d2361882e
Jannat <jannatbajwa.sia@gmail.com>
2026-01-29 18:30:54 -0500

    eh started piano

---
+++ Assets/Script/KeyPlay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class KeyPlay : MonoBehaviour
+{
+
+    public AudioSource sound;
+    private Rect audiorect;
+
+    void Start()
+    {
+        audiorect = new Rect(20, 20, 100, 20);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(audiorect, "Play Audio");
+
+        if (audiorect.Contains(Event.current.mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                sound.Play();
+            }
+
+
+        }
+    }
+}
