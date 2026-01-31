1d2fad87e4ba271ef9a61d32b81c79f3709f511b
jannatb11 <jannatbajwa.sia@gmail.com>
2026-01-30 19:48:34 -0500

    Working click button keys!

--- Assets/Script/KeyPlay.cs
+++ Assets/Script/KeyPlay.cs
@@ -2,36 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
-public class KeyPlay : MonoBehaviour
+public class ButtonSound : MonoBehaviour
 {
+    public AudioSource audioSource; 
+    public AudioClip clip;           
 
-    public AudioSource sound;
-    private Rect audiorect;
-
-    void Start()
+    public void Play()
     {
-        audiorect = new Rect(20, 20, 100, 20);
-    }
-
-
-    void Update()
-    {
-
-    }
-
-    void OnGUI()
-    {
-        GUI.Label(audiorect, "Play Audio");
-
-        if (audiorect.Contains(Event.current.mousePosition))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                sound.Play();
-            }
-
-
-        }
+        if (audioSource != null && clip != null)
+            audioSource.PlayOneShot(clip);
     }
 }
