8fa619738e46c43c53914e060c5d1ba9ed09bcd1
jannatb11 <jannatbajwa.sia@gmail.com>
2026-01-30 21:48:06 -0500

    working puzzle
    only in debug log, butto needs work

--- Assets/Script/KeyPlay.cs
+++ Assets/Script/KeyPlay.cs
@@ -1,17 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonSound : MonoBehaviour
 {
-    public AudioSource audioSource; 
-    public AudioClip clip;           
+    public int buttonID;
+    public AudioSource audioSource;
+    public AudioClip clip;
 
-    public void Play()
+    private static int[] correctOrder = { 4, 6, 11 };
+    private static int currentIndex = 0;
+    private static bool puzzleSolved = false;
+    private static bool retryShown = false;
+
+    private GameObject retryButton;
+
+    private void Awake()
+    {
+        retryButton = GameObject.Find("Retry"); // automatically find your Retry button
+        if (retryButton != null)
+            retryButton.SetActive(false); // hide at start
+    }
+
+    public void Press()
     {
         if (audioSource != null && clip != null)
             audioSource.PlayOneShot(clip);
+
+        if (puzzleSolved) return;
+
+        if (buttonID == correctOrder[currentIndex])
+        {
+            currentIndex++;
+
+            if (currentIndex >= correctOrder.Length)
+            {
+                puzzleSolved = true;
+                if (retryButton != null)
+                    retryButton.SetActive(false); // hide retry when solved
+                Debug.Log("Puzzle Solved!");
+            }
+        }
+        else
+        {
+            currentIndex = 0;
+
+            if (!retryShown && retryButton != null)
+            {
+                retryButton.SetActive(true); // show retry after wrong press
+                retryShown = true;
+                Debug.Log("Retry button activated!");
+            }
+
+            Debug.Log("Wrong! Try Again!");
+        }
+
+        Debug.Log("Pressed: " + buttonID);
+    }
+
+    public void Retry()
+    {
+        currentIndex = 0;
+        puzzleSolved = false;
+        retryShown = false;
+
+        if (retryButton != null)
+            retryButton.SetActive(false); // hide Retry again
+
+        Debug.Log("Puzzle Reset");
     }
 }
