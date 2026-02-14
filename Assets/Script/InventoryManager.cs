6e9ce0277aa685c2763783b8c91232d09a491d75
Jannat <jannatbajwa.sia@gmail.com>
2026-02-14 12:09:49 -0500

    daisyinv

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -5,17 +5,27 @@ public class InventoryManager : MonoBehaviour
     public GameObject daisyinv;
     public GameObject pianoinv;
     public GameObject mouseinv;
+    public GameObject daisyinv2;
 
     void Start()
     {
-        daisyinv.SetActive(PlayerPrefs.GetInt("DaisyCollected", 0) == 1);
+        //daisyinv.SetActive(PlayerPrefs.GetInt("DaisyCollected", 0) == 1);
+        daisyinv.SetActive(false);
+        daisyinv2.SetActive(true);
     }
 
 
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
+        
+        
+    }
+
+    public void SwitchUI()
+    {
+        daisyinv2.SetActive(false);
+        daisyinv.SetActive(true);
     }
 
 }
-   
