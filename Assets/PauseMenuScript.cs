5fb6cb808e0e7543efa694b9d2953c1960cb85d6
Rio H <Riohayesdiaz.sia@gmail.com>
2026-05-02 10:31:54 -0400

    Auto stash before merge of "new-main" and "origin/new-main"

--- Assets/PauseMenuScript.cs
+++ Assets/PauseMenuScript.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PauseMenuScript : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P)){
-            Debug.Log("Pause");
+            if(isPaused){
+                isPaused = false;
+                Debug.Log("unpause");
+            } else{
+                isPaused = true;
+                Debug.Log("pause");
+            }
         }
     }
 }
